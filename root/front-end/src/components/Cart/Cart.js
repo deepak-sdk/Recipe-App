7a5500e1c27663c4a893687e4e8047b758beeaa7
deepak-sdk <itisdeepakkumar@outlook.com>
2021-12-21 23:47:27 +0530

    REMOVEITEMCART

--- root/front-end/src/components/Cart/Cart.js
+++ root/front-end/src/components/Cart/Cart.js
@@ -13,7 +13,9 @@ const Cart = (props) => {
 
   const hasItem = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id)
+  };
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({...item, amount:1})
   };
